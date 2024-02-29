ac930045814331c3913fc7d78f6c08a31d2e2c9f
Anthony Li <antli@seas.upenn.edu>
2024-02-29 16:28:11 -0500

    Document all the things! (and fully rename pass to skip)

--- Guesstaurant/View Models/GameViewModel.swift
+++ Guesstaurant/View Models/GameViewModel.swift
@@ -10,23 +10,41 @@ import CoreMotion
 import MapKit
 import SwiftUI
 
+/// The game's view model.
 class GameViewModel: ObservableObject {
+    /// The current state of the game. See ``GameState`` for a list of possible values.
     @Published private(set) var state = GameState.loading
-    @Published private(set) var score = 0
-    fileprivate(set) var restaurants = [MKMapItem]()
     
+    /// The number of restaurants the player got correct.
+    @Published private(set) var score = 0
+    
+    /// A list of restaurants to rotate between. Populated by the ``fetchPlaces(search:)`` method.
+    fileprivate var restaurants = [MKMapItem]()
+    
+    // TODO: Add properties and methods as needed
+    
+    /// Starts loading the game by requesting location access, or requesting the location itself if access has
+    /// already been granted.
     func loadGame() {
         state = .loading
         // TODO: Fill this in during step 2
     }
     
+    /// Starts the game itself by setting the state to ready and starting motion updates.
+    ///
+    /// Called when `fetchPlaces` finishes loading restaurants.
     func startGame() {
         // TODO: Fill this in during steps 4 and 5
     }
     
+    /// Updates the game's state based on new motion data.
+    /// 
+    /// Should be called whenever the game receives a motion update.
+    /// 
+    /// - Parameter motion: The latest device motion data.
     func handleMotion(_ motion: CMDeviceMotion) {
         switch state {
-        case .ready, .correct, .pass:
+        case .ready, .correct, .skip:
             // TODO: Fill this in during step 6
             break
         case .restaurant(_):
@@ -40,6 +58,11 @@ class GameViewModel: ObservableObject {
 }
 
 fileprivate extension GameViewModel {
+    /// Starts looking for places that match the given search, adding them to the `restaurants` array.
+    ///
+    /// This method returns immediately, but calls ``startGame()`` when the search completes.
+    ///
+    /// - Parameter search: An MKLocalSearch object describing the places to search for.
     func fetchPlaces(search: MKLocalSearch) {
         Task {
             do {
